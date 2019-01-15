8b50fb22e066cd941474f245da8336f2429bfbd7
Kitu83 <amat.alexandre@gmail.com>
2019-01-15 20:53:27 +0100

    Create config.php

---
+++ ajax/config.php
@@ -0,0 +1,6 @@
+<?php
+  $dbname = "..."; // Name of database.
+  $dbhost = "..."; // Host of database.
+  $dbuser = "..."; // User of database.
+  $dbpasswd = "..."; // Password of database.
+  $dbprefix = "..."; // Prefix of MySQL tables name.
